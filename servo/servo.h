44610aa82a806b48dfc8cf87e606f84bc1e16ca0
kk030509 <127388576+kk030509@users.noreply.github.com>
2026-06-08 22:21:56 +0900

    Add files via upload

---
+++ servo/servo.h
@@ -0,0 +1,16 @@
+﻿#ifndef SERVO_H
+#define SERVO_H
+#include <avr/io.h>
+// 상태 정의
+typedef enum {
+	SERVO_IDLE,
+	SERVO_OPENING,
+	SERVO_WAIT,
+	SERVO_CLOSING
+} SERVO_STATE;
+// 함수
+void Servo_Init(void);
+void Servo_Start(void);
+void Servo_Update(void);
+void HandleServo(int count);
+#endif
